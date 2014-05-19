6196a5903b3115caecbc006cac185f60938ea52c
Bryan Folliot <bfolliot@clever-age.com>
2014-05-19 13:52:06 +0200

    Add composer.json and empty class (Slugify\Slugify)

---
+++ src/Slugify/Slugify.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bfolliot\Slugify;
+
+class Slugify
+{
+
+    /**
+     * Slugify a string
+     * 
+     * @param string $string the string to slugify
+     * 
+     * @return string
+     */
+    public function slugify($string)
+    {
+        return $string;
+    }
+}
